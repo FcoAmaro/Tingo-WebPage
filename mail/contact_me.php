186063db2a77f3bdfdbe6b9e0ea8458dd50e9656
Unknown <cisc.amaro@gmail.com>
2017-09-27 18:24:53 -0300

    Update 3
    Imagenes actualizadas

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,7 +14,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'francisco.diaz.13@sansano.usm.cl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'cisc.amaro@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Ha recibido un nuevo mensaje a su sitio web\n\n"."Detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
 $headers = "From: noresponder@tingoid.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
